238f2d116aea4ac93a704ee7c6858b3ff2a5aba9
SBarn11 <sarabarnett1@outlook.com>
2022-01-24 22:38:17 -0700

    initial commit and create User model routes

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create the User model
+class User extends Model {}
+
+//define table columns and configuration
+User.init(
+  {
+    //define and id column
+    id: {
+      //use the special sequelize dataupes object to provide what type of data it is
+      type: DataTypes.INTEGER,
+      //equivalent on sql not null
+      allowNull: false,
+      //instruct that this is the Prinary key
+      primaryKey: true,
+      //turn on auto increment
+      autoIncrement: true
+    },
+    //define username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    //define and email column 
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //no duplicate emails allowed
+      unique: true,
+      //if allownull is set to false, we can run our data through the validator before the table is created
+      validate: {
+        isEmail: true
+      }
+    },
+    //define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //this means the password muse be at least 4 characters
+        len: [4]
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports= User;
