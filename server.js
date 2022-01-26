8c7e4bae32608a1d0044a4710f35ef5bbcc5841d
SBarn11 <sarabarnett1@outlook.com>
2022-01-25 20:56:40 -0700

    start of module 14 oops

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+//css stylesheet stuff
+// const path = require('path');
+
+//handlebars stuff
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+//
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//css stylesheet path
+// app.use(express.static(path.join(__dirname, 'public')));
+//
 
 //turn on routes
 app.use(routes);
