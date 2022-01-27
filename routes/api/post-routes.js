8771a8396ee6d727255dc2561c74bbb0e2f0795f
SBarn11 <sarabarnett1@outlook.com>
2022-01-27 12:21:04 -0700

    add vote count to posts and add model method

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -7,7 +7,7 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
   console.log('===============');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     include: [
       {
         model: User,
@@ -29,7 +29,7 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -67,35 +67,15 @@ router.post('/', (req, res) => {
 
 //update a post by vote
 router.put('/upvote', (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  })
-  .then(() => {
-    //find post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        //use raw SQL function query to get a count of how many votes
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-  .then(dbPostData => res.json(dbPostData))
+  //custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+  .then(updatedPostData => res.json(updatedPostData))
   .catch(err => {
     console.log(err);
     res.status(400).json(err);
     });
-  });
 });
+
 
 //update post title
 router.put('/:id', (req, res) => {
