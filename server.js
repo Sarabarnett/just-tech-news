1713b99266af32ad008150e8fdbed2e3964ee0d1
SBarn11 <sarabarnett1@outlook.com>
2022-01-27 18:23:36 -0700

    homepage

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
-//css stylesheet stuff
-// const path = require('path');
-
 //handlebars stuff
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-//
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+//css stylesheet stuff
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
 
 //css stylesheet path
-// app.use(express.static(path.join(__dirname, 'public')));
-//
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 //turn on routes
 app.use(routes);
